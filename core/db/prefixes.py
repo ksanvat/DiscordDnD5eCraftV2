eed9bbd431b6c2ca1b40d982a66f1375a07f62f7
Aleksandr Vatulin <ksanworks@gmail.com>
2021-09-11 09:32:45 +0300

    Update prefixes.py

--- core/db/prefixes.py
+++ core/db/prefixes.py
@@ -21,12 +21,29 @@ DATA = [
             'Психический',
         ],
         value_args=[
-            '2 [dmg prefix t1]',
-            '4 [dmg prefix t2]',
-            '6 [dmg prefix t3]',
-            '1d2 [dmg prefix t1]',
-            '1d4 [dmg prefix t2]',
-            '1d6 [dmg prefix t3]',
+            '+2 урона [t1]',
+            '+4 урона [t2]',
+            '+6 урона [t3]',
+            '+1d2 урона [t1]',
+            '+1d4 урона [t2]',
+            '+1d6 урона [t3]',
+        ],
+    ),
+    Group(
+        tags=ItemTags(weapon=True),
+        type_args=['Критический'],
+        value_args=[
+            '+1 к диапазону крита [t1]',
+            '+2 к диапазону крита [t2]',
+        ],
+    ),
+    Group(
+        tags=ItemTags(weapon=True),
+        type_args=['Меткий'],
+        value_args=[
+            '+1 на попадание [t1]',
+            '+2 на попадание [t2]',
+            '+3 на попадание [t3]',
         ],
     ),
 ]
