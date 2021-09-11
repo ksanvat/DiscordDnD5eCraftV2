162a9e167982cca6c673bd9d2754caad8cc00336
Aleksandr Vatulin <ksanworks@gmail.com>
2021-09-11 18:10:34 +0300

    add info

--- core/db/suffixes/weapon.py
+++ core/db/suffixes/weapon.py
@@ -3,4 +3,11 @@ from core.types import ItemTags
 
 
 DATA = [
+    # Group(
+    #     tags=ItemTags(weapon=True),
+    #     type_args=[''],
+    #     value_args=[
+    #         '',
+    #     ],
+    # ),
 ]
