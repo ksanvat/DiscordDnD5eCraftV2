3b4ea63d6908bb28fad34d82678f9d4e4da03e0f
Aleksandr Vatulin <ksanworks@gmail.com>
2021-09-11 22:13:37 +0300

    feat

--- core/db/implicits/jewellery.py
+++ core/db/implicits/jewellery.py
@@ -5,7 +5,9 @@ from core.types import ItemTags
 DATA = [
     Group(
         tags=ItemTags(jewellery=True),
-        type_args=['TODO'],
-        value_args=['TODO'],
+        type_args=['Усиленная Концентрация'],
+        value_args=[
+            'раз в день вы можете использовать две концентрации вместо одной',
+        ],
     ),
 ]
