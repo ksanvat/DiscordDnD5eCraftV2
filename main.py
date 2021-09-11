da9886fc25016ba57f8ff3dd6e23284fdddfb8ae
Aleksandr Vatulin <ksanworks@gmail.com>
2021-09-11 06:06:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 
 class Client(discord.Client):
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message) -> None:
+        if message.author == self.user:
+            return
+
+        print(f'Echo "{message.content}"')
 
 
 if __name__ == '__main__':
